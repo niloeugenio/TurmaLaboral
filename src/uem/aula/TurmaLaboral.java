1b767e1692b7ac6ee12b4db49d12d19a88619fe1
AlfredoSebastiao <stiaoalfredo@gmail.com>
2017-08-29 14:53:08 -0700

    Primeiro Commit

---
+++ src/uem/aula/TurmaLaboral.java
@@ -0,0 +1,17 @@
+
+package uem.aula;
+
+/**
+ *
+ * @author Alfredo
+ */
+public class TurmaLaboral {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
